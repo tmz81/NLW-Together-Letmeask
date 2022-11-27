ec1f1d104f76c927dd0e03d76dd41ef7d0e31230
tmz81 <tmz_81@hotmail.com>
2022-11-27 18:47:18 -0300

    refactoring 🚀

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,39 +1,42 @@
-import { FormEvent, useState } from 'react'
-import { Link, useHistory } from 'react-router-dom'
-import illustrationImg from '../assets/img/illustration.svg'
-import logoImg from '../assets/img/logo.svg';
-import { Button } from '../components/Button';
-import { database } from '../services/firebase';
-import { useAuth } from '../hooks/useAuth';
+import { FormEvent, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import illustrationImg from "../assets/img/illustration.svg";
+import logoImg from "../assets/img/logo.svg";
+import { Button } from "../components/Button";
+import { database } from "../services/firebase";
+import { useAuth } from "../hooks/useAuth";
 
-import '../styles/auth.scss';
+import "../styles/auth.scss";
 
 export function NewRoom() {
-  const { user } = useAuth()
-  const history = useHistory()
-  const [newRoom, setNewRoom] = useState('');
+  const { user } = useAuth();
+  const history = useHistory();
+  const [newRoom, setNewRoom] = useState("");
 
   async function handleCreateRoom(event: FormEvent) {
     event.preventDefault();
 
-    if (newRoom.trim() === '') {
+    if (newRoom.trim() === "") {
       return;
     }
 
-    const roomRef = database.ref('rooms');
+    const roomRef = database.ref("rooms");
 
     const firebaseRoom = await roomRef.push({
       title: newRoom,
       authorId: user?.id,
-    })
+    });
 
-    history.push(`/rooms/${firebaseRoom.key}`)
+    history.push(`/rooms/${firebaseRoom.key}`);
   }
 
   return (
     <div id="page-auth">
       <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
         <strong>Toda pergunta tem uma resposta.</strong>
         <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
       </aside>
@@ -42,15 +45,13 @@ export function NewRoom() {
           <img src={logoImg} alt="Letmeask" />
           <h2>Criar uma nova sala</h2>
           <form onSubmit={handleCreateRoom}>
-            <input 
+            <input
               type="text"
-              placeholder="Nome da sala"
-              onChange={event => setNewRoom(event.target.value)}
+              placeholder="  Nome da sala"
+              onChange={(event) => setNewRoom(event.target.value)}
               value={newRoom}
             />
-            <Button type="submit">
-              Criar sala
-            </Button>
+            <Button type="submit">Criar sala</Button>
           </form>
           <p>
             Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
@@ -58,5 +59,5 @@ export function NewRoom() {
         </div>
       </main>
     </div>
-  )
-};
+  );
+}
